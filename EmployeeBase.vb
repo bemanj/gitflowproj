16432f5d4dd581d17b57c28f3e47f2cf6b33d862
Robert John Millano <bemanj87@gmail.com>
2018-01-10 19:11:23 +0800

    upload files

---
+++ EmployeeBase.vb
@@ -0,0 +1,16 @@
+﻿Public Class EmployeeBase
+    Private _lastName As String
+    Public Property LastName() As String
+        Get
+            Return _lastName
+        End Get
+        Set(ByVal value As String)
+            _lastName = value
+        End Set
+    End Property
+
+    Sub AddEmployee()
+        MsgBox("New Employee added")
+    End Sub
+
+End Class
